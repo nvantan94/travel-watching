bf9e317044357630ed9d3b5cdfd29e0cd85d44f0
tannv_57 <NVantan94@gmail.com>
2020-11-12 12:56:48 +0900

    add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the Adventure newsletter to receive our best vacation deals
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscribe at any time
+        </p>
+        <div className="input-areas">
+          <form action="">
+            <input
+              type="email"
+              className="footer-input"
+              placeholder="Your Email"
+            />
+            <Button buttonStyle="btn--outline">Subscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms Of Service</Link>
+          </div>
+
+          <div className='footer-link-items'>
+                <h2>Contact Us</h2>
+                <Link to='/'>Contact</Link>
+                <Link to='/'>Support</Link>
+                <Link to='/'>Destinations</Link>
+                <Link to='/'>Sponsorships</Link>
+            </div>
+        </div>
+
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>Videos</h2>
+            <Link to="/sign-up">Submit Video</Link>
+            <Link to="/">Ambassadors</Link>
+            <Link to="/">Agency</Link>
+            <Link to="/">Influence</Link>
+          </div>
+          <div className="footer-link-items">
+                <h2>Social Media</h2>
+                <Link to="/sign-up">Instagram</Link>
+                <Link to="/">Facebook</Link>
+                <Link to="/">Youtube</Link>
+                <Link to="/">Twitter</Link>
+            </div>
+        </div>
+      </div>
+
+      <section className="social-media">
+          <div className="social-media-wrap">
+            <div className="footer-logo">
+              <Link className="social-logo" to="/">
+                TRVL <i className="fab fa-typo3"></i>
+              </Link>
+            </div>
+            <small className="website-rights">TRVL @ 2020</small>
+            <div className="social-icons">
+              <Link
+                className="social-icon-link facebook"
+                to="/"
+                target="_blank"
+                aria-label="Facebook"
+              >
+                <i className="fab fa-facebook-f"></i>
+              </Link>
+              <Link
+                className="social-icon-link instagram"
+                to="/"
+                target="_blank"
+                aria-label="Instagram"
+              >
+                <i className="fab fa-instagram"></i>
+              </Link>
+              <Link
+                className="social-icon-link linked"
+                to="/"
+                target="_blank"
+                aria-label="LinkedIn"
+              >
+                <i className="fab fa-linkedin"></i>
+              </Link>
+            </div>
+          </div>
+        </section>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../../components/HeroSection'
 import Cards from '../../components/Cards';
+import Footer from '../Footer';
 
 function Home() {
     return (
         <div className="home-container">
             <HeroSection />
             <Cards />
+            <Footer />
         </div>
     )
 }
